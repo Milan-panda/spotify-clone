7dfe6af545c18ee5eba48245070f53585f5761a7
milan <milanpanda4425@gmail.com>
2022-06-14 18:52:20 +0530

    basic spotify functionalities done

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.jsx";
+import "./index.css";
+import reducer, { initialState } from "./utils/reducer";
+import { StateProvider } from "./utils/StateProvider";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <StateProvider initialState={initialState} reducer={reducer}>
+      <App />,
+    </StateProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
